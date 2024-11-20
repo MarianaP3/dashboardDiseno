73be0355daedc50109e8d71cabe0a1b99e1c8854
HctrGZ <hectorgil2003.hg@gmail.com>
2024-11-19 21:51:09 -0600

    Creacion de componentes y de las dos primeras vistas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ResPaswComponent } from './components/res-pasw/res-pasw.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    {path: "respasw", component: ResPaswComponent},
+    {path: "login", component: LoginComponent}
+];
---
+++ src/app/components/res-pasw/res-pasw.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-res-pasw',
+  standalone: true,
+  imports: [FooterComponent],
+  templateUrl: './res-pasw.component.html',
+  styleUrl: './res-pasw.component.css'
+})
+export class ResPaswComponent {
+
+}
