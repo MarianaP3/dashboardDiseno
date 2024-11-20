011fffe8d4d825ef6d9c0b6ff8969c2730612f36
HctrGZ <hectorgil2003.hg@gmail.com>
2024-11-20 16:53:25 -0600

    Main header complete with 2 modals, also the creation components for the creation of the modals

---
+++ src/app/components/header-modals/notifications/notifications.component.ts
@@ -0,0 +1,18 @@
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './notifications.component.html',
+  styleUrl: './notifications.component.css'
+})
+export class NotificationsComponent {
+  @Input() isOpen = false; // Recibe el estado del modal desde el padre
+  @Output() close = new EventEmitter<void>(); // Emite evento para cerrar el modal
+
+  onClose(): void {
+    this.close.emit(); // Emite un evento al padre para cerrar el modal
+  }
+}
